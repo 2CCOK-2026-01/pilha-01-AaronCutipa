7fc64a47a29153cf782bc0b81ac7dcfec135160f
Aaron Cutipa <aaron.canaviri@sptech.school>
2026-04-14 22:11:39 -0300

    Atividade finalizada

--- src/main/java/github/britooo/InverterTextoStack.java
+++ src/main/java/github/britooo/InverterTextoStack.java
@@ -2,12 +2,24 @@ package github.britooo;
 
 public class InverterTextoStack {
     public static String inverter(String input) {
+
+        Pilha<String> pilha = new Pilha<>(input.length());
+
+        for (int i = 0; i < input.length(); i++) {
+            pilha.push(String.valueOf(input.charAt(i)));
+        }
+
+        String elemento = "";
+        for (int i = pilha.size(); i > 0; i--) {
+            elemento += pilha.pop();
+        }
+
         /*
             É óbvio que não precisamos de uma pilha para resolver esse
             problema, porém, precisamos desenvolver nosso raciocínio lógico!
 
             Então, não fique pilhado, use a classe Pilha.
         */
-        return null;
+        return elemento;
     }
 }
